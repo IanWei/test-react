b34b01e42b4b73f0151a1084b5d3dabcf69e398d
ianwei <ikaeuu@gmail.com>
2021-07-02 09:38:28 +1000

    feat: change Button

--- packages/react/lib/molecules/Button/Button.js
+++ packages/react/lib/molecules/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = () => {
-    return (React.createElement("button", null, "Click button"));
+    return React.createElement("button", null, "Click button");
 };
 
 export { Button };
