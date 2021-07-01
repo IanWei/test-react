964e9d8587fe5289342298c1168b8e67caaa9ede
ianwei <ikaeuu@gmail.com>
2021-07-01 16:44:08 +1000

    initial commit

---
+++ playground/src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Dashboard } from './features/dashboard/Dashboard';
+
+export const App = () => {
+    return <Dashboard name="Hello" />;
+};
---
+++ playground/src/features/dashboard/Dashboard.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { DashboardProps } from './Dashboard.props';
+
+export const Dashboard: FC<DashboardProps> = () => {
+    return (
+        <div>Dashboard</div>
+    )
+}
